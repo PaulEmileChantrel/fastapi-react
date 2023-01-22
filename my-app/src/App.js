96a6c89ba3683a13f35d80ed4c181658c5be6049
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-01-22 12:15:16 -0500

    adding chart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import {useState,useRef} from 'react'
 import axios from "axios";
+import { Chart } from "react-google-charts";
+
 
 function App() {
-  const [market,setMarket] = useState('');
-  const [data,setData] = useState('');
+  
+  const [data,setData] = useState(false);
+  const [apiError,setApiError] = useState(false);
   const marketRef = useRef()
 
   function callApi(){
@@ -12,15 +15,24 @@ function App() {
     if (marketInput === ''){
       return 
     } 
-    setMarket(prev=>{return marketInput})
+    
     console.log(marketInput)
 
     marketRef.current.value = null;
     const url = "http://127.0.0.1:8000/binance/"+marketInput+"/15"
     axios.get(url).then((response) => {
       console.log(response)
-      setData(response.data);
+      let time = [];
+      let close = [];
+      for (var i = 0; i < response.data.length; i++) {
+        time.push(response.data[i][0]/1000); // convert to seconds
+        close.push(response.data[i][2])
+      }
+      setApiError('')
+      console.log(time)
+      setData([['Time','Close'],time,close]);
     }).catch((e)=>{
+      setApiError(e.message)
       console.log(e)
     });
 
@@ -28,7 +40,15 @@ function App() {
   return (<>
     <input type="text" ref={marketRef}/>
     <button onClick={callApi}>Search Market</button>
-    <div>{data}</div>
+    <p>{apiError}</p>
+    <Chart
+      chartType="ScatterChart"
+      data={data}
+      width="100%"
+      height="400px"
+      legendToggle
+    />
+    
   </>
   );
 }
