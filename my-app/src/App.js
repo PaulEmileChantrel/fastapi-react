4df2441b9faa04637d97e379b88b720f231c7cf4
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-01-22 12:57:49 -0500

    added google chart to plot the data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,50 +5,69 @@ import { Chart } from "react-google-charts";
 
 
 function App() {
-  
-  const [data,setData] = useState(false);
-  const [apiError,setApiError] = useState(false);
+  const [market,SetMarket]=useState('');
+  const [data,setData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [apiError,setApiError] = useState('');
   const marketRef = useRef()
+  const daysRef = useRef()
 
   function callApi(){
     const marketInput = marketRef.current.value;
+    let daysInput = daysRef.current.value;
     if (marketInput === ''){
       return 
     } 
+    if (daysInput === ''){
+      daysInput = 100 
+    } 
     
     console.log(marketInput)
-
+    SetMarket(marketInput)
     marketRef.current.value = null;
-    const url = "http://127.0.0.1:8000/binance/"+marketInput+"/15"
+    daysRef.current.value = null;
+    const url = "http://127.0.0.1:8000/binance/"+marketInput+"/"+daysInput
     axios.get(url).then((response) => {
       console.log(response)
-      let time = [];
-      let close = [];
+      if (response.data.code===-1100 || response.data.code===-1121){
+      setApiError("Invalide Symbole")
+    return }
+      let time_close = [["Time","Close"]];
+      
       for (var i = 0; i < response.data.length; i++) {
-        time.push(response.data[i][0]/1000); // convert to seconds
-        close.push(response.data[i][2])
+        time_close.push([new Date(parseInt(response.data[i][0])),parseInt(response.data[i][2])]); // convert to seconds
+        
       }
+      
       setApiError('')
-      console.log(time)
-      setData([['Time','Close'],time,close]);
+      setShow(true)
+      setData(time_close);
     }).catch((e)=>{
-      setApiError(e.message)
+      if (e.message === 'Network Error') {
+        setApiError("API not connected")
+      }
+      
       console.log(e)
     });
 
   }
+  
   return (<>
-    <input type="text" ref={marketRef}/>
+    <input type="text" ref={marketRef} placeholder="market"/>
+    <input type="text" ref={daysRef} placeholder="days"/>
     <button onClick={callApi}>Search Market</button>
     <p>{apiError}</p>
+    { show ?
     <Chart
-      chartType="ScatterChart"
+      chartType="LineChart"
       data={data}
       width="100%"
       height="400px"
       legendToggle
+        
     />
-    
+    : null}
+     
   </>
   );
 }
